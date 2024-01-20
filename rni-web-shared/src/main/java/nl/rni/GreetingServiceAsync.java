b0b83a09169729c8b6f08a86e98fe6d5a7911a6c
Hua Zhang <hzhang1978@gmail.com>
2024-01-20 16:58:32 +0100

    hello world

---
+++ rni-web-shared/src/main/java/nl/rni/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package nl.rni;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+    void greetServer(String input, AsyncCallback<GreetingResponse> callback) throws IllegalArgumentException;
+}
